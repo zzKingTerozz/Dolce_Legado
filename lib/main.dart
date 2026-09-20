e526b728dcb535bb4904d62517eed40cb024b6e8
Kelvin Duarte <kelvin_duarte2003@outlook.com>
2026-09-20 18:21:09 -0400

    Prueba: Desactiva Firebase temporalmente

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:dolce_legado/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart'; 
+import 'package:google_fonts/google_fonts.dart'; // <-- Agrega este import
+import 'firebase_options.dart';
 
 // Falta importar la ruta de tu archivo SplashScreen. 
 // Para que VS Code lo haga solo, haz clic sobre la palabra "SplashScreen" más abajo, presiona "Ctrl + ." y selecciona "Import library..."
@@ -9,12 +10,14 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  GoogleFonts.config.allowRuntimeFetching = true;
 
-  // Cambio de MyApp() por DolceLegadoApp()
-  runApp(const DolceLegadoApp()); 
+  // COMENTA ESTAS LÍNEAS PARA DESACTIVAR FIREBASE TEMPORALMENTE
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
+
+  runApp(const DolceLegadoApp());
 }
 
 class DolceLegadoApp extends StatelessWidget {
